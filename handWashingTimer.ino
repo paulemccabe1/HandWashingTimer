f5734f4fe63e49fa5d603817fe5d22042cc4ed0a
Paul McCabe <paulemccabe1@gmail.com>
2020-03-21 20:42:58 -0400

    Arduino Sketch for thr No Touch Hand Washing Timer

---
+++ handWashingTimer.ino
@@ -0,0 +1,267 @@
+/*Paul McCabe 3/21/2020 
+ *
+ *using Moz's 4 Digit 7 Segmetn Display example and Isaac100's ultrasonic sensor example.
+ *code write by Moz for YouTube changel LogMaker360, 5-11-2016
+ *code belongs to this video, https://www.youtube.com/watch?v=256VQ6eVNng
+ *
+ * HC-SR04 example sketch
+ *
+ * https://create.arduino.cc/projecthub/Isaac100/getting-started-with-the-hc-sr04-ultrasonic-sensor-036380
+ *
+ * by Isaac100
+ */
+
+//For Display
+int digit1 = 6; //Thousands Place
+int digit2 = 9; //Hundreds Place
+int digit3 = 10; //Tens Place
+int digit4 = 11; //Ones Place
+
+#define DIGIT_ON  LOW
+#define DIGIT_OFF  HIGH
+
+int segA = 2; //Top
+int segB = 3; //Top Right
+int segC = 4; //Bottom Right
+int segD = 5; //Bottom
+int segE = A0;//Bottom Left
+int segF = 7; //Top Left
+int segG = 8; //Middle
+
+int counter =20;
+int timer = 50;
+int resetTimer = 50;
+int endCounter = 11;
+
+//For Sonar
+const int trigPin = A1;
+const int echoPin = A2;
+
+float duration, distance;
+
+int ledPin = 13;
+
+void setup() {                
+  
+  //For Display
+  pinMode(segA, OUTPUT);
+  pinMode(segB, OUTPUT);
+  pinMode(segC, OUTPUT);
+  pinMode(segD, OUTPUT);
+  pinMode(segE, OUTPUT);
+  pinMode(segF, OUTPUT);
+  pinMode(segG, OUTPUT);
+
+  pinMode(digit1, OUTPUT);
+  pinMode(digit2, OUTPUT);
+  pinMode(digit3, OUTPUT);
+  pinMode(digit4, OUTPUT);
+  Serial.begin(9600);
+  
+  //For Sonar
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  
+}
+void loop()
+{
+  //For Sonar
+  digitalWrite(ledPin, LOW);
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration*.0343)/2;
+  //Serial.print("Distance: ");
+  //Serial.println(distance);
+  delay(100);
+  if (distance <= 5) 
+  {
+    countDownFrom20(counter, timer);
+  }  
+ 
+}
+
+//Digit Disply Numbers
+void lightNumber(int numberToDisplay) {
+
+#define SEGMENT_ON  HIGH
+#define SEGMENT_OFF LOW
+
+  switch (numberToDisplay){
+
+  case 0:
+    digitalWrite(segA, SEGMENT_ON);
+    digitalWrite(segB, SEGMENT_ON);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_ON);
+    digitalWrite(segE, SEGMENT_ON);
+    digitalWrite(segF, SEGMENT_ON);
+    digitalWrite(segG, SEGMENT_OFF);
+    break;
+
+  case 1:
+    digitalWrite(segA, SEGMENT_OFF);
+    digitalWrite(segB, SEGMENT_ON);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_OFF);
+    digitalWrite(segE, SEGMENT_OFF);
+    digitalWrite(segF, SEGMENT_OFF);
+    digitalWrite(segG, SEGMENT_OFF);
+    break;
+
+  case 2:
+    digitalWrite(segA, SEGMENT_ON);
+    digitalWrite(segB, SEGMENT_ON);
+    digitalWrite(segC, SEGMENT_OFF);
+    digitalWrite(segD, SEGMENT_ON);
+    digitalWrite(segE, SEGMENT_ON);
+    digitalWrite(segF, SEGMENT_OFF);
+    digitalWrite(segG, SEGMENT_ON);
+    break;
+
+  case 3:
+    digitalWrite(segA, SEGMENT_ON);
+    digitalWrite(segB, SEGMENT_ON);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_ON);
+    digitalWrite(segE, SEGMENT_OFF);
+    digitalWrite(segF, SEGMENT_OFF);
+    digitalWrite(segG, SEGMENT_ON);
+    break;
+
+  case 4:
+    digitalWrite(segA, SEGMENT_OFF);
+    digitalWrite(segB, SEGMENT_ON);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_OFF);
+    digitalWrite(segE, SEGMENT_OFF);
+    digitalWrite(segF, SEGMENT_ON);
+    digitalWrite(segG, SEGMENT_ON);
+    break;
+
+  case 5:
+    digitalWrite(segA, SEGMENT_ON);
+    digitalWrite(segB, SEGMENT_OFF);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_ON);
+    digitalWrite(segE, SEGMENT_OFF);
+    digitalWrite(segF, SEGMENT_ON);
+    digitalWrite(segG, SEGMENT_ON);
+    break;
+
+  case 6:
+    digitalWrite(segA, SEGMENT_ON);
+    digitalWrite(segB, SEGMENT_OFF);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_ON);
+    digitalWrite(segE, SEGMENT_ON);
+    digitalWrite(segF, SEGMENT_ON);
+    digitalWrite(segG, SEGMENT_ON);
+    break;
+
+  case 7:
+    digitalWrite(segA, SEGMENT_ON);
+    digitalWrite(segB, SEGMENT_ON);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_OFF);
+    digitalWrite(segE, SEGMENT_OFF);
+    digitalWrite(segF, SEGMENT_OFF);
+    digitalWrite(segG, SEGMENT_OFF);
+    break;
+
+  case 8:
+    digitalWrite(segA, SEGMENT_ON);
+    digitalWrite(segB, SEGMENT_ON);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_ON);
+    digitalWrite(segE, SEGMENT_ON);
+    digitalWrite(segF, SEGMENT_ON);
+    digitalWrite(segG, SEGMENT_ON);
+    break;
+
+  case 9:
+    digitalWrite(segA, SEGMENT_ON);
+    digitalWrite(segB, SEGMENT_ON);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_ON);
+    digitalWrite(segE, SEGMENT_OFF);
+    digitalWrite(segF, SEGMENT_ON);
+    digitalWrite(segG, SEGMENT_ON);
+    break;
+
+  case 10:
+    digitalWrite(segA, SEGMENT_OFF);
+    digitalWrite(segB, SEGMENT_OFF);
+    digitalWrite(segC, SEGMENT_OFF);
+    digitalWrite(segD, SEGMENT_OFF);
+    digitalWrite(segE, SEGMENT_OFF);
+    digitalWrite(segF, SEGMENT_OFF);
+    digitalWrite(segG, SEGMENT_OFF);
+    break;
+    
+  case 11:
+    digitalWrite(digit1, DIGIT_ON);
+    digitalWrite(digit2, DIGIT_ON);
+    digitalWrite(digit3, DIGIT_ON);
+    digitalWrite(digit4, DIGIT_ON);
+    digitalWrite(segA, SEGMENT_OFF);
+    digitalWrite(segB, SEGMENT_OFF);
+    digitalWrite(segC, SEGMENT_OFF);
+    digitalWrite(segD, SEGMENT_OFF);
+    digitalWrite(segE, SEGMENT_OFF);
+    digitalWrite(segF, SEGMENT_OFF);
+    digitalWrite(segG, SEGMENT_ON);
+    
+    delay(3000);
+
+    digitalWrite(segA, SEGMENT_OFF);
+    digitalWrite(segB, SEGMENT_OFF);
+    digitalWrite(segC, SEGMENT_OFF);
+    digitalWrite(segD, SEGMENT_OFF);
+    digitalWrite(segE, SEGMENT_OFF);
+    digitalWrite(segF, SEGMENT_OFF);
+    digitalWrite(segG, SEGMENT_OFF);
+    break;
+      
+  }
+ 
+}
+
+void countDownFrom20(int counter, int timer)
+{
+   //For Display
+      while (counter != -1)
+      {
+      //Serial.print("Counter:");
+      //Serial.println(counter);
+      digitalWrite(digit1, DIGIT_OFF);
+      digitalWrite(digit2, DIGIT_OFF);
+  
+        while(timer != 0)
+        {                 
+          digitalWrite(digit3, DIGIT_ON);    //Tens Place
+          digitalWrite(digit4, DIGIT_OFF);
+          lightNumber((counter/10)%10);
+          delay(10);
+    
+          digitalWrite(digit3, DIGIT_OFF);    //Ones Place
+          digitalWrite(digit4, DIGIT_ON);
+          lightNumber((counter%10));
+          delay(10);
+    
+          timer=timer-1;
+        }
+      timer=resetTimer;
+      counter=counter-1;
+      Serial.println(counter);
+
+      }
+      lightNumber(11);
+  
+}
